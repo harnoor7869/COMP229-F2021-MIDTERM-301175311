a4228edf6048784bfafcea3dfb9d2985cee167c7
Harnoor kaur <hkaur773@my.centennialcollege.ca>
2021-10-29 23:00:32 -0400

    update

--- server/routes/books.js
+++ server/routes/books.js
@@ -28,8 +28,8 @@ router.get('/add', (req, res, next) => {
             return console.error(err);
         } else {
             res.render('books/details', {
-                title: "Add Book",
-                books: Addbooks,
+                title: 'New Book',
+                books: books,
                 Title: book.Title,
                 Price: book.Price,
                 Author: book.Author,
@@ -65,61 +65,71 @@ router.post('/add', (req, res, next) => {
 
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
-
-  /*****************
-   * ADD CODE HERE *
-   *****************/
-   let id = req.params.id;
-   book.findById(id, (err, bookToEdit) => {
-     if (err) res.end(err);
-     else {
-       res.render("books/details", {
-         title: "Book list",
-         books: bookToEdit,
-       });
-     }
-   });
+    console.log(req.params.id);
+    book.findById(req.params.id.value,(err, books) => {
+        if (err) {
+            return console.error(err);
+        } else {
+            res.render('books/details', {
+                title: "Edit Book",
+                books: book,
+                Title: book.Title,
+               
+                Price: book.Price,
+                Author: book.Author,
+                Genre: book.Genre
+            });
+        }
+    });
 });
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
 
-  /*****************
-   * ADD CODE HERE *
-   *****************/
-   let id = req.params.id;
-   const {Title, Price, Author, Genre} = req.body;
- 
-   const updatedBook = new book({
-     _id: id,
-     Title,
-     Price,
-     Author,
-     Genre,
-   });
-   book.updateOne({_id: id}, updatedBook, (err) => {
-     if (err) {
-       res.end(err);
-     } else {
-       res.redirect("/books");
-     }
-   });
- });
+    const new_book = new book({
+        Title: req.body.title_txt,
+        
+        Price: req.body.price_txt,
+        Author: req.body.auther_txt,
+        Genre: req.body.genre_txt
+    });
+    new_book.save().then(() => console.log('book saved !'));
+    book.find((err, books) => {
+        if (err) {
+            return console.error(err);
+        } else {
+            res.render('books/index', {
+                title: 'Books',
+                books: books
+            });
+        }
+    });
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+
+});
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
-
-  /*****************
-   * ADD CODE HERE *
-   *****************/
-   let id = req.params.id;
-   book.deleteOne({_id: id}, (err) => {
-     if (err) res.end(err);
-     else res.redirect("/books");
-   });
- });
- 
+    book.findById(id,(err, books) => {
+        if (err) {
+            return console.error(err);
+        } else {
+            res.render('books/details', {
+                title: 'Edit Book',
+                books: books,
+                Title: book.Title,
+                
+                Price: book.Price,
+                Author: book.Author,
+                Genre: book.Genre
+            });
+        }
+    });
+});
 
 
 module.exports = router;
+
 
